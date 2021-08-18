ef1b6b5a4b8831fc0de001fe7f1de6ac7baef372
Varun Sharma <varun.pr0coder@gmail.com>
2021-08-18 23:21:36 +0530

    medhavi bhudiaraja

--- routes/playRoutes.js
+++ routes/playRoutes.js
@@ -12,6 +12,7 @@ router.get('/play/feelok', (req, res) => {res.render('itsokaynottofeelokay')});
 router.get('/play/kaala', (req, res) => {res.render('kaala')});
 router.get('/play/4runner', (req, res) => {res.render('4runner')});
 router.get('/play/thelibrarian', (req, res) => {res.redirect('https://pastebin.com/6KGNd3qE')});
+router.get('/play/david', (req, res) => {res.render('david')});
 router.get('/leaderboard', playController.leaderboard_get);
 router.get('/getQuestion', requireAuth, stopBanned, levelInfo, playController.question_get);
 
